ca19612163ab7c9e1626671f066375323578eb67
LucaSantosSP <lucas-santos20.17@hotmail.com>
2022-08-26 19:31:51 -0300

    Teste com tupla

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-let list: number[] = [1, 2, 3];
+/*let list: number[] = [1, 2, 3];
 
 let otherList: Array<number> = [1, 2, 3];
 
@@ -7,4 +7,14 @@ appDiv.innerHTML = `
 <h1>Array declaration</h1>
 <h2>list: ${list}</h2>
 <h2>other List: ${otherList}</h2>
+`;*/
+
+let me: [string, number];
+
+me = ['Lucas', 22];
+
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `
+<h1>Tuple</h1>
+<h2> Me chamo ${me[0]} e tenho ${me[1]} anos</h2>
 `;
