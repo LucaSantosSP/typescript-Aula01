ae888b7c7d0f43e086f1a18339afc8c36934c731
LucaSantosSP <lucas-santos20.17@hotmail.com>
2022-08-26 20:48:35 -0300

    Implementado interval com funticon

--- index.ts
+++ index.ts
@@ -1,55 +1,4 @@
-/*let list: number[] = [1, 2, 3];
-
-let otherList: Array<number> = [1, 2, 3];
-
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `
-<h1>Array declaration</h1>
-<h2>list: ${list}</h2>
-<h2>other List: ${otherList}</h2>
-`;
-
-let me: [string, number];
-
-me = ['Lucas', 22];
-
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `
-<h1>Tuple</h1>
-<h2> Me chamo ${me[0]} e tenho ${me[1]} anos</h2>
-`;
-
-enum Color {
-  Red,
-  Green,
-  Blue,
-}
-let color1 = Color.Green;
-
-enum ColorWithRef {
-  Red = '10',
-  Green = '20',
-  Blue = 'Teste',
-}
-let color2 = ColorWithRef.Blue;
-
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `
-  <h1>Enum<h1>
-  <h2>Cor 1: ${color1}</h2>
-  <h2>Cor 2: ${color2}</h2>
-`;
-
-let notSure: any = 4;
-notSure = 'Maybe I am a string';
-notSure = false;
-
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `
-  <h1>Any Variable</h1>
-  <h2>${notSure}</h2>
-`;
-function warnUser(): void {
+/*function warnUser(): void {
   console.log('This is my warning message');
 }
 
@@ -85,7 +34,7 @@ appDiv.innerHTML = `
   <h1>Type assertions</h1>
   <h2>${strLenght}</h2>
   <h2>${strLenght2}</h2>
-`;*/
+`;
 
 function sum(n1: number, n2: number): number {
   return n1 + n2;
@@ -102,3 +51,23 @@ appDiv.innerHTML = `
   <h2>Soma de 11 e 329: ${sum(11, 329)}</h2>
   <h2>Meu nome completo: ${fullName('Lucas', 'Santos')}</h2>
 `;
+
+let title: string = 'Olá, mundo!';
+let paragraph: string = 'Blablabla..';
+
+let counter = 0;
+
+let intervalId = setInterval(() => {
+  counter = counter + 1;
+  const appP: HTMLElement = document.getElementById('app.p');
+  appP.innerHTML = 'Contador: ' + counter;
+}, 1000);
+
+const btn = document.getElementById('app.btn');
+btn?.addEventListener('click', alertMethod);
+function alertMethod(this: HTMLElement, ev: Event) {
+  alert('Alertando com typescript');
+}
+
+const appH1: HTMLElement = document.getElementById('app.h1');
+appH1.innerHTML = title;*/
