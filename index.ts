f5a2e29317a42bbbec1718356add1098d0667d7f
LucaSantosSP <lucas-santos20.17@hotmail.com>
2022-08-26 19:36:46 -0300

    Implementando teste com enum

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ appDiv.innerHTML = `
 <h1>Array declaration</h1>
 <h2>list: ${list}</h2>
 <h2>other List: ${otherList}</h2>
-`;*/
+`;
 
 let me: [string, number];
 
@@ -17,4 +17,25 @@ const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `
 <h1>Tuple</h1>
 <h2> Me chamo ${me[0]} e tenho ${me[1]} anos</h2>
+`;*/
+
+enum Color {
+  Red,
+  Green,
+  Blue,
+}
+let color1 = Color.Green;
+
+enum ColorWithRef {
+  Red = 10,
+  Green = 20,
+  Blue = 30,
+}
+let color2 = ColorWithRef.Blue;
+
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `
+  <h1>Enum<h1>
+  <h2>Cor 1: ${color1}</h2>
+  <h2>Cor 2: ${color2}</h2>
 `;
